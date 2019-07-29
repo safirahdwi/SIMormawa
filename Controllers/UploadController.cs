d2db116e543ee17744f1d20356415c8cec931aee
Dwika Ayu Novianti <dwikaayunvnti@gmail.com>
2019-07-29 18:04:09 +0700

    Nambah fungsi upload file

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ormawa.Services;
+using Ormawa.ViewModel;
 
 namespace Ormawa.Controllers
 {
@@ -13,26 +15,38 @@ namespace Ormawa.Controllers
     {
         private IHostingEnvironment _env;
         private string _dir;
+        private readonly IFileService _fileService;
 
-        public UploadController(IHostingEnvironment env)
+        public UploadController(IHostingEnvironment env, IFileService fileService)
         {
             _env = env;
             _dir = _env.ContentRootPath;
+            _fileService = fileService;
         }
         public IActionResult Index() => View();
 
-
-        public IActionResult MultipleFiles(IEnumerable<IFormFile> files)
+        [HttpPost]
+        public async Task<IActionResult> MultipleFiles(UploadViewModel vm)
         {
-            int i = 0;
-            foreach (var file in files)
+
+            try
             {
-                using (var fileStream = new FileStream(Path.Combine(_dir, $"file{i++}.pdf"), FileMode.Create, FileAccess.Write))
-                {
-                    file.CopyTo(fileStream);
-                }
+                if (vm.FileDokumen == null || vm.FileDokumen.Length == 0)
+                    return Content("file not selected");
+
+                var namaFile = Path.GetFileName(vm.FileDokumen.FileName);
+                var dok = namaFile.Substring(0, namaFile.IndexOf('.'));
+
+                vm.Urldokumen = $"https://{await _fileService.UploadDokumen($"test_{dok}", vm.FileDokumen)}";
+                
+                
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("Index");
+            catch
+            {
+                
+                return RedirectToAction("Index");
+            }
         }
     }
 }

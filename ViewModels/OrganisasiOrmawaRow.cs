6c5f105cd36b089b655f9e5ff417cb9f4c12cc03
Dwika Ayu Novianti <dwikaayunvnti@gmail.com>
2019-07-29 21:59:44 +0700

    add viewmodel

---
+++ ViewModels/OrganisasiOrmawaRow.cs
@@ -0,0 +1,28 @@
+﻿using Mvc.JQuery.DataTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ormawa.ViewModels
+{
+    public class OrganisasiOrmawaRow
+    {
+        [DataTables(Visible =false)]
+        public int Id { get; set; }
+        [DataTables]
+        public string Nama { get; set; }
+        [DataTables]
+        public string NamaEn { get; set; }
+        [DataTables]
+        public string NomorSk { get; set; }
+        [DataTables]
+        public DateTime? Tmt { get; set; }
+        [DataTables]
+        public DateTime? Tst { get; set; }
+        [DataTables]
+        public string JenisOrganisasi { get; set; }
+        [DataTables]
+        public string Aksi { get; set; }
+    }
+}
---
+++ ViewModels/OrganisasiOrmawaVM.cs
@@ -0,0 +1,20 @@
+﻿using Mvc.JQuery.DataTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ormawa.ViewModels
+{
+    public class OrganisasiOrmawaVM
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string NamaEn { get; set; }
+        public string NomorSk { get; set; }
+        public DateTime? Tmt { get; set; }
+        public DateTime? Tst { get; set; }
+        public string JenisOrganisasi { get; set; }
+        public DataTableConfigVm DataTableConfigVm { get; set; }
+    }
+}

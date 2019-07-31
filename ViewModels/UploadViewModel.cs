be2956101baff8f879f86f609e3f8c2dbe6e6418
annisamonitha <annisamonitha12@gmail.com>
2019-07-31 16:39:07 +0700

    Fungsi Publikasi

---
+++ ViewModels/UploadViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ormawa.ViewModels
+{
+    public class UploadViewModel
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Urldokumen { get; set; }
+        public int? JenisDokumenId { get; set; } 
+        public IFormFile FileDokumen { get; set; }
+
+    }
+    }

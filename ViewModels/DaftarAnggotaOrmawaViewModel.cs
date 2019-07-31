549164e7947e7a37e155e50de7fbc682946372f2
safirah988@gmail.com <safirah988@gmail.com>
2019-07-31 17:27:22 +0700

    Fungsi untuk menampilkan DaftarAnggota

--- ViewModels/DaftarAnggotaOrmawaViewModel.cs
+++ ViewModels/DaftarAnggotaOrmawaViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Mvc.JQuery.DataTables;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,11 @@ namespace Ormawa.ViewModels
     public class DaftarAnggotaOrmawaViewModel
     {
         public int Id { get; set; }
-        public int? MahasiswaId { get; set; }
-        public int? OrganisasiOrmawaId { get; set; }
+        public int MahasiswaId { get; set; }
+        public int OrganisasiOrmawaId { get; set; }
         public DateTime? TanggalBergabung { get; set; }
         public bool? StatusAnggota { get; set; }
-
-        //Mahasiswa
-        public int? OrangId { get; set; }
-        public int? StrataId { get; set; }
-        public string Nimkey { get; set; }
+        public DataTableConfigVm DataTableConfigVm { get; set; }
 
         public IEnumerable<SelectListItem> ListMahasiswa { get; set; }
         public IEnumerable<SelectListItem> ListOrmawa { get; set; }

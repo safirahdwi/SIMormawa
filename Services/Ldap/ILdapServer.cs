cad79ca52d4d09fe86ee92730671abd3487c61c2
safirah988@gmail.com <safirah988@gmail.com>
2019-08-09 11:17:58 +0700

    menambahkan ldap services

---
+++ Services/Ldap/ILdapServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPB.Ldap
+{
+    public interface ILdapServer
+    {
+        LdapUser Authenticate(string username, string password);
+        string Filter { get; set; }
+    }
+}
---
+++ Services/Ldap/LdapAuthenticationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPB.Ldap
+{
+    public class LdapAuthenticationException : Exception
+    {
+        public LdapAuthenticationException()
+        {
+        }
+
+        public LdapAuthenticationException(string message) : base(message)
+        {
+        }
+    }
+}
